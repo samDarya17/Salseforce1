ca50c756e8653486e784cafbecfb2d5484aee9a7
Zalivko Denis <zalivkodenis@gmail.com>
2023-07-11 18:44:09 +0300

    1

--- src/test/java/pages/DetailsPage.java
+++ src/test/java/pages/DetailsPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class DetailsPage extends BasePage{
-    public static final By DETAILS_TITLE =By.xpath("//a[@data-label='Details']");
+    public static final By DETAILS_TITLE =By.xpath("//a[@data-label ='Details']");
     public DetailsPage(WebDriver driver) {
         super(driver);
     }
